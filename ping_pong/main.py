0d485a342ffe1dddb97d5f69044016ccc2b57c91
rpopov94 <rpopov@jinr.ru>
2020-09-05 18:31:06 +0300

    corner passge

--- ping_pong/main.py
+++ ping_pong/main.py
@@ -2,4 +2,14 @@ import turtle
 
 window = turtle.Screen()
 window.title("Ping-Pong")
-window.setup(width=1.0, height=1.0)
+window.setup(width=0.5, height=0.5)
+window.bgcolor("black")
+
+border = turtle.Turtle()
+border.color("green")
+
+border.goto(-250, 150)
+border.goto(250, 150)
+border.goto(250, -150)
+border.goto(-250, -150)
+window.mainloop()
