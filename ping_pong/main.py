8e434db0b0bdc241f949ff5cf592b4925a77a7fb
rpopov94 <rpopov@jinr.ru>
2020-09-05 18:50:32 +0300

    add speed

--- ping_pong/main.py
+++ ping_pong/main.py
@@ -6,6 +6,7 @@ window.setup(width=0.5, height=0.5)
 window.bgcolor("black")
 
 border = turtle.Turtle()
+border.speed(5)
 border.color("green")
 
 border.begin_fill()
@@ -17,5 +18,7 @@ border.goto(-250, 150)
 border.end_fill()
 
 border.goto(0, 300)
+border.color('white')
+border.goto(0, -300)
 
 window.mainloop()
