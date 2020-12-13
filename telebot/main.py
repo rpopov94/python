5db0a8f54ef9891b95a9938ec170fcb31c8a98f9
Roman <rpopov@jinr.ru>
2020-12-13 20:38:36 +0300

    переписал бот с помощью aiogram

--- telebot/main.py
+++ telebot/main.py
@@ -1,74 +1,29 @@
-import requests
-import json
-import time
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-import const
-
-
-def answer_user_bot(data):
-    data = {
-        'chat_id': const.MY_ID,
-        'text': data
-    }
-    url = const.URL.format(
-        token=const.TOKEN,
-        method=const.SEND_METH
-    )
-    response = requests.post(url, data=data)
+from config import TOKEN
+from weather import get_weather
 
 
-def parse_weather_data(data):
-    for elem in data['weather']:
-        weather_state = elem['main']
-    temp = round(data['main']['temp'] - 273.15, 2)
-    city = data['name']
-    msg = f'The weather in {city}: Temp is {temp}, State is {weather_state}'
-    return msg
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 
-def get_weather(location):
-    url = const.WEATHER_URL.format(city=location,
-                                   token=const.WEATHER_TOKEN)
-    response = requests.get(url)
-    if response.status_code != 200:
-        return 'city not found'
-    data = json.loads(response.content)
-    return parse_weather_data(data)
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне название города!")
 
 
-def get_message(data):
-    return data['message']['text']
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Этот бот предоставляет актуальные данные о погоде")
 
 
-def save_update_id(update):
-    with open(const.UPDATE_ID_FILE_PATH, 'w') as file:
-        file.write(str(update['update_id']))
-    const.UPDATE_ID = update['update_id']
-    return True
-
-
-def main():
-    while True:
-        url = const.URL.format(token=const.TOKEN, method=const.UPDATE_METH)
-        content = requests.get(url).text
-
-        data = json.loads(content)
-        result = data['result'][::-1]
-        needed_part = None
-
-        for elem in result:
-            if elem['message']['chat']['id'] == const.MY_ID:
-                needed_part = elem
-                break
-
-        if const.UPDATE_ID != needed_part['update_id']:
-            message = get_message(needed_part)
-            msg = get_weather(message)
-            answer_user_bot(msg)
-            save_update_id(needed_part)
-
-        time.sleep(1)
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, get_weather(msg.text))
 
 
 if __name__ == '__main__':
-    main()
+    executor.start_polling(dp)
