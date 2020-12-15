2cbc7f201218d55e422f1bcf436f459b50510e7a
Roman <rpopov@jinr.ru>
2020-12-15 21:22:40 +0300

    завершил минимальный план

--- telebot/main.py
+++ telebot/main.py
@@ -2,30 +2,38 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-from config import TOKEN
-from weather import smart_function
+import config
+import weather
 
-
-bot = Bot(token=TOKEN)
+bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
-request = ''
+
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне название города!")
+    await message.reply("Привет!\nНапиши мне название города!\nДля получения справки введите\n/help")
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Этот бот предоставляет актуальные данные о погоде," \
-                        "если не плучается найти город, введите название на латинице")
+    await message.reply("Этот бот предоставляет актуальные данные о погоде,\n" \
+                        "если не плучается найти город,\n "
+                        "введите название на латинице\n" \
+                        "для получение истории\n" \
+                        "[город] -h [yyyy-mm-dd]\n" \
+                        "Прогноз погоды на 3 дня\n" \
+                        "[город] -f\n" \
+                        "Текущая погода\n" \
+                        "[город]", \
+                        reply=False
+                        )
 
 
 @dp.message_handler(content_types=types.ContentTypes.TEXT)
 async def do_answer(mes: types.Message):
     text = mes.text
     if text and not text.startswith('/'):
-        await mes.reply(text=smart_function(text), reply=False)
+        await mes.reply(text=weather.smart_function(text), reply=False)
 
 
 if __name__ == '__main__':
--- telebot/weather.py
+++ telebot/weather.py
@@ -14,12 +14,12 @@ def current_weather(location):
     state = parse_data['current']['condition']['text']
     time = parse_data['location']['localtime']
     wind = parse_data['current']["wind_dir"]
-    return f'Current time: {time} \n' \
-           f'Weather of {location} is \n' \
-           f'Temperature is: {temperature} \n' \
-           f'State: {state} \n' \
-           f'Pressure: {parse_data["current"]["pressure_mb"] * 0.75} mm\n' \
-           f'Wind: {parse_data["current"]["wind_kph"]} km/h, {wind}\n'
+    return f'Текушее время {time} \n' \
+           f'Погода {location} is \n' \
+           f'Температура {temperature} \n' \
+           f'{state} \n' \
+           f'Давление: {parse_data["current"]["pressure_mb"] * 0.75} mm\n' \
+           f'Ветер: {parse_data["current"]["wind_kph"]} km/h, {wind}\n'
 
 
 def history(location, period):
@@ -32,21 +32,22 @@ def history(location, period):
     max_temp = data['forecast']['forecastday'][0]['day']['maxtemp_c']
     avg_temp = data['forecast']['forecastday'][0]['day']['avgtemp_c']
     min_temp = data['forecast']['forecastday'][0]['day']['mintemp_c']
-#    pressure = data['forecast']['forecastday'][0]['hour']['12']['pressure_mb'] * 0.75
+    pressure = data['forecast']['forecastday'][0]['hour'][12]['pressure_mb'] * 0.75
     wind = data['forecast']['forecastday'][0]['day']['maxwind_kph']
-    return f'For {period} \n' \
-           f'State {state}\n' \
-           f'Max temperature {max_temp}\n' \
-           f'Min temperature {min_temp}\n' \
-           f'Average temperature {avg_temp}\n' \
-           f'Velocity wind {wind}\n' \
-           f'Average pressure \n'
+    return f'Дата {period} \n' \
+           f'{state}\n' \
+           f'Днем {max_temp}\n' \
+           f'Вечером  {min_temp}\n' \
+           f'Средняя температура {avg_temp}\n' \
+           f'Скорость ветра {wind}\n' \
+           f'Давление {pressure}'
+
 
 
 def forecast(location):
     type_req = 'forecast'
     url = W_URL.format(type_req, KEY, location)
-    url += '&days=3'                            # 3 days that condition for free plan
+    url += '&days=3'  # 3 days that condition for free plan
     data = requests.get(url).json()
     date_list, temp_list, state_list = [], [], []
     temp = data['forecast']['forecastday']
@@ -54,19 +55,22 @@ def forecast(location):
         date_list.append(temp[i]['date'])
         temp_list.append(temp[i]['day']['avgtemp_c'])
         state_list.append(temp[i]['day']['condition']['text'])
-    return f"Forecast for three days in {location}\n" \
-           f"{date_list[0]}\nAverage temp {temp_list[0]}\n" \
-           f"State {state_list[0]}\n"\
-           f"{date_list[1]}\nAverage temp {temp_list[1]}\n" \
-           f"State {state_list[1]}\n" \
-           f"{date_list[2]}\nAverage temp {temp_list[2]}\n" \
-           f"State {state_list[2]}"
+    return f"Прогноз на 3 дня в  {location}\n" \
+           f"{date_list[0]}\nСредняя температура {temp_list[0]}С\n" \
+           f"{state_list[0]}\n" \
+           f"{date_list[1]}\nСредняя температура {temp_list[1]}С\n" \
+           f"{state_list[1]}\n" \
+           f"{date_list[2]}\nСредняя температура {temp_list[2]}С\n" \
+           f"{state_list[2]}"
 
 
-def smart_function(location, fore=None, hist=None, period=None):
-    if fore is None and hist is None:
-        return current_weather(location)
-    elif fore == 'forecast' and hist is None:
-        return forecast(location)
+def smart_function(query):
+    answer = query
+    if '-h' in query:
+        answer = history(query, query[-10:])
+    elif '-f' in query:
+        answer = forecast(query)
     else:
-        return history(location, period)
+        answer = current_weather(query)
+    return answer
+
