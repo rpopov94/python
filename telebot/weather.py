5f8492feb20fb3fc7d332072819b1234dcd64a14
Roman <rpopov@jinr.ru>
2020-12-15 16:40:19 +0300

    добавил функцию прогноза

--- telebot/weather.py
+++ telebot/weather.py
@@ -18,7 +18,7 @@ def current_weather(location):
            f'Weather of {location} is \n' \
            f'Temperature is: {temperature} \n' \
            f'State: {state} \n' \
-           f'Pressure: {parse_data["current"]["pressure_mb"] * 0.750064} mm\n' \
+           f'Pressure: {parse_data["current"]["pressure_mb"] * 0.75} mm\n' \
            f'Wind: {parse_data["current"]["wind_kph"]} km/h, {wind}\n'
 
 
@@ -28,5 +28,37 @@ def history(location, period):
     url += '&dt={}'.format(period)
     response = requests.get(url)
     data = response.json()
-    temp = data['forecast']['forecastday'][0]['day']['avgtemp_c']
-    return temp
+    state = data['forecast']['forecastday'][0]['day']['condition']['text']
+    max_temp = data['forecast']['forecastday'][0]['day']['maxtemp_c']
+    avg_temp = data['forecast']['forecastday'][0]['day']['avgtemp_c']
+    min_temp = data['forecast']['forecastday'][0]['day']['mintemp_c']
+#    pressure = data['forecast']['forecastday'][0]['hour']['12']['pressure_mb'] * 0.75
+    wind = data['forecast']['forecastday'][0]['day']['maxwind_kph']
+    return f'For {period} \n' \
+           f'State {state}\n' \
+           f'Max temperature {max_temp}\n' \
+           f'Min temperature {min_temp}\n' \
+           f'Average temperature {avg_temp}\n' \
+           f'Velocity wind {wind}\n' \
+           f'Average pressure \n'
+
+
+def forecast(location):
+    type_req = 'forecast'
+    url = W_URL.format(type_req, KEY, location)
+    url += '&days=3'                            # 3 days that condition for free plan
+    data = requests.get(url).json()
+    date_list, temp_list, state_list = [], [], []
+    temp = data['forecast']['forecastday']
+    for i in range(3):
+        date_list.append(temp[i]['date'])
+        temp_list.append(temp[i]['day']['avgtemp_c'])
+        state_list.append(temp[i]['day']['condition']['text'])
+    return f"Forecast for three days in {location}\n" \
+           f"{date_list[0]}\nAverage temp {temp_list[0]}\n" \
+           f"State {state_list[0]}\n"\
+           f"{date_list[1]}\nAverage temp {temp_list[1]}\n" \
+           f"State {state_list[1]}\n" \
+           f"{date_list[2]}\nAverage temp {temp_list[2]}\n" \
+           f"State {state_list[2]}"
+
